4a421b1841061aa3529b053340e237401d8df612
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-24 23:09:35 +0530

    updated

--- src/ClassifyXProvider.tsx
+++ src/ClassifyXProvider.tsx
@@ -1,3 +1,11 @@
+//
+// ClassifyX
+// by ImKKingshuk
+// Git-  https://github.com/ImKKingshuk/ClassifyX.git
+// Copyright © 2023 , @ImKKingshuk | All Rights Reserved.
+// GNU General Public License v3.0 or later
+//
+
 import React, { createContext, ReactNode } from "react";
 import ClassifyX, { ClassValue } from "./ClassifyX";
 
