b56a3d026e69b4e466bf9b143e3880c160c0d339
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-24 22:52:27 +0530

    updated

---
+++ src/ClassifyXProvider.tsx
@@ -0,0 +1,44 @@
+import React, { createContext, ReactNode } from "react";
+import ClassifyX, { ClassValue } from "./ClassifyX";
+
+type ClassifyXContextType = {
+  ClassifyX: (...args: ClassValue[]) => string;
+  bind: (...args: ClassValue[]) => string;
+  has: (...args: ClassValue[]) => boolean;
+  concat: (...args: ClassValue[]) => string;
+  toggle: (base: string, condition: boolean, className: string) => string;
+  unique: (...args: ClassValue[]) => string;
+  prefix: (prefix: string, ...args: ClassValue[]) => string;
+  cssModule: (module: Record<string, string>, ...args: ClassValue[]) => string;
+};
+
+const ClassifyXContext = createContext<ClassifyXContextType | undefined>(
+  undefined
+);
+
+type ClassifyXProviderProps = {
+  children: ReactNode;
+};
+
+export const ClassifyXProvider: React.FC<ClassifyXProviderProps> = ({
+  children,
+}: ClassifyXProviderProps) => {
+  return (
+    <ClassifyXContext.Provider
+      value={{
+        ClassifyX,
+        bind: ClassifyX.bind,
+        has: ClassifyX.has,
+        concat: ClassifyX.concat,
+        toggle: ClassifyX.toggle,
+        unique: ClassifyX.unique,
+        prefix: ClassifyX.prefix,
+        cssModule: ClassifyX.cssModule,
+      }}
+    >
+      {children}
+    </ClassifyXContext.Provider>
+  );
+};
+
+export default ClassifyXContext;
