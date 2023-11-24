f7c52d6bb8e40a090ced8b089e8538265867dff5
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-25 00:58:22 +0530

    updated

--- src/ClassifyXProvider.tsx
+++ src/ClassifyXProvider.tsx
@@ -5,7 +5,7 @@
 // Copyright © 2023 , @ImKKingshuk | All Rights Reserved.
 // GNU General Public License v3.0 or later
 //
-
+// src/ClassifyXProvider.tsx
 import React, { createContext, ReactNode } from "react";
 import ClassifyX, { ClassValue } from "./ClassifyX";
 
@@ -18,6 +18,12 @@ type ClassifyXContextType = {
   unique: (...args: ClassValue[]) => string;
   prefix: (prefix: string, ...args: ClassValue[]) => string;
   cssModule: (module: Record<string, string>, ...args: ClassValue[]) => string;
+  spacing: (size: number) => string;
+  flex: (direction: string, align: string, justify: string) => string;
+  flexItem: (flex: number) => string;
+  fontSize: (size: number) => string;
+  util: (utility: string) => string;
+  textAlign: (align: string) => string;
 };
 
 const ClassifyXContext = createContext<ClassifyXContextType | undefined>(
@@ -42,6 +48,12 @@ export const ClassifyXProvider: React.FC<ClassifyXProviderProps> = ({
         unique: ClassifyX.unique,
         prefix: ClassifyX.prefix,
         cssModule: ClassifyX.cssModule,
+        spacing: ClassifyX.spacing,
+        flex: ClassifyX.flex,
+        flexItem: ClassifyX.flexItem,
+        fontSize: ClassifyX.fontSize,
+        util: ClassifyX.util,
+        textAlign: ClassifyX.textAlign,
       }}
     >
       {children}
